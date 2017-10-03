3498d43e73a3205bdf6c212a765ab24e3798616d
Jesse Vincent <jesse@keyboard.io>
2017-10-02 18:59:44 -0700

    Remove a couple of C preprocessor macros involved in the definition of keyboard macros, in the hope of making the code clearer

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -24,9 +24,8 @@
 #include "Kaleidoscope-Model01-TestMode.h"
 
 #define MACRO_VERSION_INFO 1
-#define Macro_VersionInfo M(MACRO_VERSION_INFO)
 #define MACRO_ANY 2
-#define Macro_Any M(MACRO_ANY)
+
 #define NUMPAD_KEYMAP_ID 2
 
 #define FUNCTION_KEYMAP  KEYMAP_STACKED ( \
@@ -56,7 +55,7 @@ ___ \
                  Key_Keymap1_Momentary, \
 \
 \
-    Macro_VersionInfo,  ___, Key_Keypad7, Key_Keypad8,   Key_Keypad9,        Key_KeypadSubtract, ___, \
+    M(MACRO_VERSION_INFO),  ___, Key_Keypad7, Key_Keypad8,   Key_Keypad9,        Key_KeypadSubtract, ___, \
     ___, ___, Key_Keypad4, Key_Keypad5,   Key_Keypad6,        Key_KeypadAdd,      ___, \
          ___, Key_Keypad1, Key_Keypad2,   Key_Keypad3,        Key_Equals,         Key_Quote, \
     ___, ___, Key_Keypad0, Key_KeypadDot, Key_KeypadMultiply, Key_KeypadDivide,   Key_Enter, \
@@ -72,7 +71,7 @@ ___ \
     Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,         \
                           Key_Keymap1_Momentary,     \
 \
-    Macro_Any,       Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_KeypadNumLock, \
+    M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_KeypadNumLock, \
     Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,       \
                    Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,       \
     Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,       \
