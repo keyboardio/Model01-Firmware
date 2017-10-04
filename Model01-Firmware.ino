a988fac8115a28a34b54e23897b106254116f51b
Craig Disselkoen <cdisselk@eng.ucsd.edu>
2017-10-03 22:41:36 -0700

    Fix misleading indentation in all three layers' keymaps
    plus add astyle directives to make astyle not complain about it

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -117,6 +117,11 @@ enum { MACRO_VERSION_INFO,
 
 enum { QWERTY, FUNCTION, NUMPAD }; // layers
 
+/* This comment temporarily turns off astyle's indent enforcement
+ *   so we can make the keymaps actually resemble the physical key layout better
+ */
+// *INDENT-OFF*
+
 const Key keymaps[][ROWS][COLS] PROGMEM = {
 
   [QWERTY] = KEYMAP_STACKED
@@ -129,7 +134,7 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    \
    M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_KeypadNumLock, \
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,        \
-   Key_H,         Key_J, Key_K, Key_L,     Key_Semicolon, Key_Quote,                        \
+                  Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,         \
    Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,         \
    Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,                             \
    ShiftToLayer(FUNCTION)),
@@ -144,7 +149,7 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    \
    Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,  \
    Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,  \
-   Key_LeftArrow,              Key_DownArrow,          Key_UpArrow,              Key_RightArrow,           ___,              ___,                       \
+                               Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,      \
    Key_PcApplication,          Key_Mute,               Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe, \
    ___, ___, Key_Enter, ___,                                                                                                                            \
    ___),
@@ -160,12 +165,14 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    \
    M(MACRO_VERSION_INFO),  ___, Key_Keypad7, Key_Keypad8,   Key_Keypad9,        Key_KeypadSubtract, ___,       \
    ___,                    ___, Key_Keypad4, Key_Keypad5,   Key_Keypad6,        Key_KeypadAdd,      ___,       \
-   ___,                         Key_Keypad1, Key_Keypad2,   Key_Keypad3,        Key_Equals,         Key_Quote, \
+                           ___, Key_Keypad1, Key_Keypad2,   Key_Keypad3,        Key_Equals,         Key_Quote, \
    ___,                    ___, Key_Keypad0, Key_KeypadDot, Key_KeypadMultiply, Key_KeypadDivide,   Key_Enter, \
    ___, ___, ___, ___,                                                                                         \
    ___)
 };
 
+/* Re-enable astyle's indent enforcement */
+// *INDENT-ON*
 
 /** versionInfoMacro handles the 'firmware version info' macro
  *  When a key bound to the macro is pressed, this macro
