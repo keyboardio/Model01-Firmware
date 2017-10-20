ee7d2e963c8435d9f2f444a83777c4b43ca15a3b
Steve Pomeroy <steve@staticfree.info>
2017-10-20 13:50:59 -0400

    Add mouse scroll wheel mapping
    This scrolls with the natural scrolling style, moving with the movement
    of the page, not the scrollbar.

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -142,10 +142,10 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    ShiftToLayer(FUNCTION)),
 
   [FUNCTION] =  KEYMAP_STACKED
-  (___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           XXX,
-   Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
-   Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
-   Key_End,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
+  (___,      Key_F1,           Key_F2,      Key_F3,            Key_F4,        Key_F5,           XXX,
+   Key_Tab,  ___,              Key_mouseUp, Key_mouseScrollDn, Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
+   Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR,        Key_mouseBtnL, Key_mouseWarpNW,
+   Key_End,  Key_PrintScreen,  Key_Insert,  Key_mouseScrollUp, Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
    ___, Key_Delete, ___, ___,
    ___,
 
