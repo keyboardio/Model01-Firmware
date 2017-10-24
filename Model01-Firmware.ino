23d0fdb01cdfeb57ac0c0c108296afbe0377ceb8
Andrew Gallagher <andrew.gallagher@siren.io>
2017-10-24 11:09:06 +0100

    Properly homed Apple/Kinesis embedded numpad layout

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -165,11 +165,11 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    ___, ___, ___, ___,
    ___,
 
-   M(MACRO_VERSION_INFO),  ___, Key_Keypad7, Key_Keypad8,   Key_Keypad9,        Key_KeypadSubtract, ___,
-   ___,                    ___, Key_Keypad4, Key_Keypad5,   Key_Keypad6,        Key_KeypadAdd,      ___,
-                           ___, Key_Keypad1, Key_Keypad2,   Key_Keypad3,        Key_Equals,         Key_Quote,
-   ___,                    ___, Key_Keypad0, Key_KeypadDot, Key_KeypadMultiply, Key_KeypadDivide,   Key_Enter,
-   ___, ___, ___, ___,
+   M(MACRO_VERSION_INFO),  ___,           Key_Clear,       Key_KeypadEquals,   Key_KeypadDivide,   Key_KeypadMultiply, ___,
+   ___,                    ___,           Key_Keypad7,     Key_Keypad8,        Key_Keypad9,        Key_KeypadSubtract, ___,
+                           Key_Backspace, Key_Keypad4,     Key_Keypad5,        Key_Keypad6,        Key_KeypadAdd,      ___,
+   ___,                    ___,           Key_Keypad1,     Key_Keypad2,        Key_Keypad3,        Key_KeypadEnter,    ___,
+   ___, ___, Key_Keypad0, Key_KeypadDot,
    ___)
 };
 
