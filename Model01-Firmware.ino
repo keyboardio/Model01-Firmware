4abf4cf2068c6874d9f3215445ae798a59752390
Andrew Gallagher <andrew.gallagher@siren.io>
2017-10-24 16:09:54 +0100

    Rationalized Shift/Alt/Command keys

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -131,14 +131,14 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    Key_Backtick,                 Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_Backslash,                Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_NonUsBackslashAndPipe,    Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
+   Key_LeftControl, Key_Backspace, Key_LeftShift, Key_LeftAlt,
    ShiftToLayer(FUNCTION),
 
    Key_Minus,     Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_Equals,
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_LeftBracket,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_RightBracket,
-   Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
+   Key_RightGui,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_RightBracket,
+   Key_RightAlt, Key_RightShift, Key_Spacebar, Key_RightControl,
    ShiftToLayer(FUNCTION)),
 
   [FUNCTION] =  KEYMAP_STACKED
