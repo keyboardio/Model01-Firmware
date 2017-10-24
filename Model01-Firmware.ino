2055900b804d3422036dfdaeacb1379f6876cb21
Andrew Gallagher <andrew.gallagher@siren.io>
2017-10-24 11:31:48 +0100

    Properly homed mousekeys w/ left button on pinky for easy drag/drop; extra PGUP/PGDN keys on Fn-Tab/Esc

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -142,10 +142,10 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    ShiftToLayer(FUNCTION)),
 
   [FUNCTION] =  KEYMAP_STACKED
-  (___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           XXX,
-   Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
-   Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
-   Key_End,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
+  (___,      Key_F1,           Key_F2,           Key_F3,         Key_F4,            Key_F5,           XXX,
+   Key_Tab,  ___,              Key_mouseWarpNW,  Key_mouseUp,    Key_mouseWarpNE,   Key_mouseWarpEnd, Key_PageUp,
+   Key_Home, Key_mouseBtnL,    Key_mouseL,       Key_mouseDn,    Key_mouseR,        Key_mouseBtnR, 
+   Key_End,  Key_PrintScreen,  Key_mouseWarpSW,  Key_mouseBtnM,  Key_mouseWarpSE,   Key_Insert,       Key_PageDown,
    ___, Key_Delete, ___, ___,
    ___,
 
