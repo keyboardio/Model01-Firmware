28ba816146abe7a56b2ecc4b636ecfde8ac5b397
Andrew Gallagher <andrew.gallagher@siren.io>
2017-11-01 23:06:12 +0000

    Refactored standard QWERTY to use modifier aliases

--- layer-std-qwerty.h
+++ layer-std-qwerty.h
@@ -10,14 +10,14 @@ KEYMAP_STACKED
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
-   ShiftToLayer(FUNCTION),
+   MOD_L_THUMB_0, MOD_L_THUMB_1, MOD_L_THUMB_2, MOD_L_THUMB_3,
+   MOD_L_PALM,
 
    M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_KeypadNumLock,
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
-   ShiftToLayer(FUNCTION))
+   MOD_BUTTERFLY, Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
+   MOD_R_THUMB_3, MOD_R_THUMB_2, MOD_R_THUMB_1, MOD_R_THUMB_0,
+   MOD_R_PALM)
 
 // *INDENT-ON*
