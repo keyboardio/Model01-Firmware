6f7a9d09f569023239e3aa6c4adf77417d1bab0b
Michael Richters <gedankenexperimenter@gmail.com>
2017-11-30 12:59:58 -0600

    Updated definition of keymaps[] to use KEYMAPS() macro
    This change will enable the `layer_count` variable for preventing
    reading past the end of the `keymaps[]` array.

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -125,7 +125,7 @@ enum { QWERTY, NUMPAD, FUNCTION }; // layers
  */
 // *INDENT-OFF*
 
-const Key keymaps[][ROWS][COLS] PROGMEM = {
+KEYMAPS(
 
   [QWERTY] = KEYMAP_STACKED
   (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
@@ -173,7 +173,7 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    ___, ___, Key_Enter, ___,
    ___)
 
-};
+	) // KEYMAPS(
 
 /* Re-enable astyle's indent enforcement */
 // *INDENT-ON*
