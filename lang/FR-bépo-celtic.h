9f01bb174ce7f5c1faac96baa66a71df25a639c1
Yoann Brosseau <yoann.brosseau@adp.com>
2018-03-06 14:04:28 +0100

    modify keymaps layouts add Bépo definitions

---
+++ lang/FR-bépo-celtic.h
@@ -0,0 +1,74 @@
+#pragma once
+
+// Alt gr
+#define Key_frb_ALTGR RALT
+
+
+// First line
+#define Key_frb_Dollar		Key_Backtick
+#define Key_frb_Quote		Key_1
+#define Key_frb_DeOQuote 	Key_2
+#define Key_frb_DeCQuote 	Key_3
+#define Key_frb_Oparenthesi	Key_4
+#define Key_frb_Cparenthesi	Key_5
+#define Key_frb_At			Key_6
+#define Key_frb_Plus		Key_7
+#define Key_frb_Minus		Key_8
+#define Key_frb_Slash		Key_9
+#define Key_frb_Multiply	Key_0
+#define Key_frb_Equals		Key_Minus
+#define Key_frb_Percent		Key_Equals
+
+
+// Second Line
+#define Key_frb_B			Key_Q
+#define Key_frb_E_Acute		Key_W
+#define Key_frb_P			Key_E
+#define Key_frb_O			Key_R
+#define Key_frb_E_Grave		Key_T
+#define Key_frb_Circumflex	Key_Y
+#define Key_frb_V			Key_U
+#define Key_frb_D			Key_I
+#define Key_frb_L			Key_O
+#define Key_frb_J			Key_P
+#define Key_frb_Z			Key_LeftBracket
+#define Key_frb_W			Key_RightBracket
+
+
+// thrid line
+#define Key_frb_A			Key_A
+#define Key_frb_U			Key_S
+#define Key_frb_I			Key_D
+#define Key_frb_E			Key_F
+#define Key_frb_Comma		Key_G
+#define Key_frb_C			Key_H
+#define Key_frb_T			Key_J
+#define Key_frb_S			Key_K
+#define Key_frb_R			Key_L
+#define Key_frb_N			Key_Semicolon
+#define Key_frb_M			Key_Quote
+#define Key_frb_C_Cedilla	Key_Backslash
+
+
+// fourth line
+#define Key_frb_E_umlaut	Key_NonUsBackslashAndPipe
+#define Key_frb_A_Grave		Key_Z
+#define Key_frb_Y			Key_X
+#define Key_frb_X			Key_C
+#define Key_frb_Period		Key_V
+#define Key_frb_K			Key_B
+#define Key_frb_Question	Key_N
+#define Key_frb_Q			Key_M
+#define Key_frb_G			Key_Comma
+#define Key_frb_H			Key_Period
+#define Key_frb_F			Key_Slash
+
+
+// Shortcut keys
+#define Key_frb_Cut			LCTRL(Key_frb_X)
+#define Key_frb_Copy		LCTRL(Key_frb_C)
+#define Key_frb_Paste		LCTRL(Key_frb_V)
+#define Key_frb_Undo		LCTRL(Key_frb_Z)
+#define Key_frb_Redo		LCTRL(Key_frb_Y)
+#define Key_frb_SelectAll	LCTRL(Key_frb_A)
+#define Key_frb_Fullscreen	Key_F11
