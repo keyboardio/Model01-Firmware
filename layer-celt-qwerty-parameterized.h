8152fec6d46f9b0d0b509db41b3550a22a18f5f8
Yoann Brosseau <yoann.brosseau@adp.com>
2018-03-06 15:18:34 +0100

    Adding QWERTY alternate layout

---
+++ layer-celt-qwerty-parameterized.h
@@ -0,0 +1,27 @@
+// Define the ABG multilingual layer
+
+/* Since this layer DOES NOT contain PgUp, PgDn or Num keys, it is recommended to
+ * use one of the ABG Fn layers so that PgUp, PgDn and Num can be accessed.
+ */
+
+/* This comment temporarily turns off astyle's indent enforcement
+ *   so we can make the keymaps actually resemble the physical key layout better
+ */
+// *INDENT-OFF*
+
+KEYMAP_STACKED
+  (___,             Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
+   Key_Quote,       Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
+   Key_PageUp,      Key_A, Key_S, Key_D, Key_F, Key_G,
+   Key_PageDown,    Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
+   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
+   ShiftToLayer(FUNCTION),
+
+   M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,      Key_0,         LockLayer(NUMPAD),
+   Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,
+                  Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,
+   Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,
+   Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
+   ShiftToLayer(FUNCTION))
+
+// *INDENT-ON*
