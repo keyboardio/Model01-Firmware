f4783b1b187dc35a8f6470668eeb4910afee4da5
Yoann Brosseau <yoann.brosseau@gmail.com>
2018-03-07 06:09:59 +0100

    Bug correction & enhancements

--- layer-celt-function.h
+++ layer-celt-function.h
@@ -6,7 +6,7 @@
 // *INDENT-OFF*
 
 KEYMAP_STACKED
-  (Key_LEDEffectNext,					___,						___,						___,						___,					___,					Key_Insert,
+  (___,					___,						___,						___,						___,					___,					Key_Insert,
    Key_Tab,				Key_Home,					Key_UpArrow,				Key_End,        			Key_PageUp,				___,					Key_Delete,
    ___,					Key_LeftArrow,				Key_DownArrow,				Key_RightArrow,				Key_PageDown,			___,
    ___,					Key_frb_Undo,				Key_frb_Cut,				Key_frb_Copy,				Key_frb_Paste,			Key_frb_Redo,			___,
@@ -17,7 +17,7 @@ KEYMAP_STACKED
    ___,    				Key_mouseBtnL,				Key_mouseUp,				Key_mouseBtnR,				Key_mouseWarpNW,		Key_mouseWarpNE,		Key_ScrollLock,
 						Key_mouseL,					Key_mouseDn,				Key_mouseR,					Key_mouseWarpSW,		Key_mouseWarpSE,		Key_Pause,
    Key_frb_Qwerty,		Consumer_Mute,				Consumer_VolumeDecrement,	Consumer_VolumeIncrement,	___,					___,					___,
-   ___, ___, ___, ___,
+   ___, Key_RightShift, Key_Spacebar, ___,
    ___)
 
 // *INDENT-ON*
