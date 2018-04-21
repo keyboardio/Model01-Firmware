84a1146f1b72611f80fdf62f39c568e0aeb42aaf
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2018-04-21 22:41:05 +0200

    fix and format layers

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -55,7 +55,8 @@ enum { QWERTY,
 
 // *INDENT-OFF*
 
-#define KEYMAP_QUERTY KEYMAP_STACKED
+KEYMAPS(
+  [QWERTY] = KEYMAP_STACKED
   (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDToggle,
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
@@ -63,10 +64,10 @@ enum { QWERTY,
    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
    ShiftToLayer(FUNCTION_LEFT),
 
-   M(MACRO_ANY),         Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_Eszett,
-   Key_Enter,            Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
-                         Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   OSL(MACROS), Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
+   M(MACRO_ANY), Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_Eszett,
+   Key_Enter,    Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,
+                 Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,
+   OSL(EMOJI),   Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,
    Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
    ShiftToLayer(FUNCTION_RIGHT)),
 
