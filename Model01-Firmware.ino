fc6bc17c4e34e878bb41434519d61e4cfa69c0d6
Olivier Lechevalier <olivier.lechevalier@gmail.com>
2018-05-24 11:14:42 +0900

    Use quote as Cmd when held

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -147,7 +147,7 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
 
    LCTRL(Key_Spacebar),  Key_6, Key_7, Key_8, Key_9, Key_0,         Key_KeypadNumLock,
    Key_Enter,     Key_J, Key_L, Key_U,     Key_Y,         Key_Semicolon, Key_Equals,
-                  Key_H, Key_N, Key_E,     Key_I,         Key_O,         Key_Quote,
+                  Key_H, Key_N, Key_E,     Key_I,         Key_O,         MT(LeftGui, Quote),
    ShiftToLayer(CODING),  Key_K, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
    OSM(RightShift), OSM(LeftAlt), Key_Spacebar, OSM(RightControl),
    ShiftToLayer(CODING)),
