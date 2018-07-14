c07eaa479b732fc55e64f087a52bdac6e801aa7c
Tre Ammatuna <tretuna@gmail.com>
2018-07-13 22:43:05 -0700

    Add QWERTY layer

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -128,7 +128,7 @@ enum {
   *
   */
 
-enum { DVORAK, NUMPAD, FUNCTION }; // layers
+enum { DVORAK, QWERTY, NUMPAD, FUNCTION }; // layers
 
 /* This comment temporarily turns off astyle's indent enforcement
  *   so we can make the keymaps actually resemble the physical key layout better
@@ -140,17 +140,34 @@ KEYMAPS(
   [DVORAK] = KEYMAP_STACKED
   (
     ___,          Key_1,          Key_2,       Key_3,      Key_4, Key_5, Key_LEDEffectNext,
-    Key_Backtick, Key_Quote,      Key_Comma,   Key_Period, Key_P, Key_Y, Key_Tab,
+    Key_Backtick, Key_Quote,      Key_Comma,   Key_Period, Key_P, Key_Y, Key_LeftBracket,
     Key_Tab,      Key_A,          Key_O,       Key_E,      Key_U, Key_I,
     Key_Escape,   Key_Semicolon,  Key_Q,       Key_J,      Key_K, Key_X, Key_Backslash,
     OSM(LeftControl), OSM(LeftAlt), OSM(LeftGui), OSM(LeftShift),
     ShiftToLayer(FUNCTION),
 
     M(MACRO_ANY),       Key_6, Key_7, Key_8, Key_9,  Key_0, LockLayer(NUMPAD),
-    Key_Enter,          Key_F, Key_G, Key_C, Key_R,  Key_L, Key_Slash,
+    Key_RightBracket,          Key_F, Key_G, Key_C, Key_R,  Key_L, Key_Slash,
                         Key_D, Key_H, Key_T, Key_N,  Key_S, Key_Equals,
     M(MACRO_FAT_ARROW), Key_B, Key_M, Key_W, Key_V,  Key_Z, Key_Minus,
     OSM(RightGui), Key_Enter, Key_Spacebar, Key_Backspace,
+    ShiftToLayer(FUNCTION)
+  ),
+
+  [QWERTY] = KEYMAP_STACKED
+  (
+    ___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
+    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_LeftBracket,
+    Key_Tab,      Key_A, Key_S, Key_D, Key_F, Key_G,
+    Key_Escape,   Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Backslash,
+    OSM(LeftControl), OSM(LeftAlt), OSM(LeftGui), OSM(LeftShift),
+    ShiftToLayer(FUNCTION),
+
+    M(MACRO_ANY),       Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
+    Key_RightBracket,   Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
+                        Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
+    M(MACRO_FAT_ARROW), Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
+    OSM(RightGui),  Key_Enter, Key_Spacebar, Key_Backspace,
     ShiftToLayer(FUNCTION)
   ),
 
@@ -173,11 +190,11 @@ KEYMAPS(
 
   [FUNCTION] =  KEYMAP_STACKED
   (
-    Key_LEDEffectNext, Key_F1, Key_F2,          Key_F3,           Key_F4,          Key_F5,        XXX,
+    LockLayer(QWERTY), Key_F1, Key_F2,          Key_F3,           Key_F4,          Key_F5,        XXX,
     Key_Tab,           Key_mouseWarpNW, Key_mouseUp,      Key_mouseWarpNE, Key_mouseBtnR, ___, ___,
     ___,               Key_mouseL,      Key_mouseDn,      Key_mouseR,      Key_mouseBtnL, ___,
     ___,               Key_mouseWarpSW, Key_mouseWarpEnd, Key_mouseWarpSE, Key_mouseBtnM, ___, ___,
-    M(MACRO_MEHA), M(MACRO_MEHB), M(MACRO_MEHC), M(MACRO_MEHD),
+    M(MACRO_MEHA), M(MACRO_MEHB), M(MACRO_MEHC), ___,
     ___,
 
     ___,  Key_F6, Key_F7,                       Key_F8,        Key_F9,                  Key_F10,                Key_F11,
