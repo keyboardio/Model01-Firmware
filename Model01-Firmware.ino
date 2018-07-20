a5b86bea5a597d6f6ddd1efe62a493727131529b
Jesse Vincent <jesse@keyboard.io>
2018-07-19 21:19:47 -0700

    Add a Caps lock key on Fn-Let by default.
    Enough customers want a caps lock that we ought to give them -something-
    in the default firmware

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -167,7 +167,7 @@ KEYMAPS(
    ___),
 
   [FUNCTION] =  KEYMAP_STACKED
-  (___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           XXX,
+  (___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           Key_CapsLock,
    Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
    Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
    Key_End,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
