ca8cd73f3ef90bf933d4feb924d35ff7cdb060c8
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2018-08-17 11:01:07 +0200

    use one shot for thump cluster modifiers

--- src/keymaps.h
+++ src/keymaps.h
@@ -19,14 +19,14 @@ KEYMAPS(
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   Key_LeftGui, Key_Backspace, Key_LeftControl, Key_LeftShift,
+   OSM(LeftGui), Key_Backspace, OSM(LeftControl), OSM(LeftShift),
    ShiftToLayer(FUNCTION_JJ),
 
    JJ_Xcode,   Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_Minus,
    Key_Enter,  Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,
                Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,
    JJ_Compose, Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Backslash,
-   Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightGui,
+   OSM(RightShift), OSM(LeftAlt), Key_Spacebar, OSM(RightGui),
    ShiftToLayer(FUNCTION_JJ)),
 
   [FUNCTION_JJ] =  KEYMAP_STACKED
